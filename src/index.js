41f50c9b5be5151b08547d357ab3dd9f9cefd603
Josh Herman <j.herman@esri.com>
2021-09-16 10:23:29 -0400

    card work

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const user = "torvalds";
+const user = "andrew";
 
 ReactDOM.render(
   <React.StrictMode>
